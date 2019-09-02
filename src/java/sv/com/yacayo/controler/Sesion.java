6a59f794ddef4e0795dade91652fd549918b0b42
josue.tobarfgkss <josue.tobarfgkss@CDS-SS-2011-004.sv.cds>
2019-09-02 15:23:32 -0600

    -- Login 002

---
+++ src/java/sv/com/yacayo/controler/Sesion.java
@@ -0,0 +1,14 @@
+package sv.com.yacayo.controler;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/** @author josue.tobarfgkss */
+@ManagedBean
+@SessionScoped
+public class Sesion { 
+    public Sesion() {
+    }
+    
+    
+}
