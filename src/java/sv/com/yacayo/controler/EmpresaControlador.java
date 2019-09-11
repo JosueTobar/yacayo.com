a1318c42ea3e5896bc9cd51183ed1b1c3f6987ad
David Cordova <David Cordova@David>
2019-09-10 21:13:59 -0600

    Correción código de login

--- src/java/sv/com/yacayo/controler/EmpresaControlador.java
+++ src/java/sv/com/yacayo/controler/EmpresaControlador.java
@@ -56,13 +56,13 @@ public class EmpresaControlador {
         } catch (Exception e) {
             return "registro?e=1";
         }
-        return "faces/views/index";
+        return "/faces/views/index?faces-redirect=true";
     }
     
     public String editar(Usuario u){
         Map<String, Object> datos= FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
         datos.put("dt", u);
-        return "perfil";
+        return "perfil?faces-redirect=true";
     }
     
     public String modificar(){
@@ -73,7 +73,7 @@ public class EmpresaControlador {
             
             uDAO.edit(SesionUtil.getUserId());
             
-            return "agregar";
+            return "agregar?faces-redirect=true";
         } catch (Exception e) {
             return null;
         }
